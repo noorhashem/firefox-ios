fef9b0d8733f4ebfc90eb3fac278e027b2cc7824
noorhashem <nour.hashem95@gmail.com>
2020-08-11 02:16:40 +0200

    setting up quick actions small widget

--- WidgetKit/QuickActionsSmall/QuickActionsSmall.swift
+++ WidgetKit/QuickActionsSmall/QuickActionsSmall.swift
@@ -2,13 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import Foundation
 import WidgetKit
-import SwiftUI
 
-@main
-struct FirefoxWidgets: WidgetBundle {
-    @WidgetBundleBuilder
-    var body: some Widget {
-        SearchQuickLinksWigdet()
-    }
-}
